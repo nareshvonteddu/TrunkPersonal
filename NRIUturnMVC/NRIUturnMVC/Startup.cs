c610d6bff530dd9774457ff986f3cc1a044bad97
naresh vonteddu <nvonteddu@hotmail.com>
2016-04-07 15:32:27 -0400

    Revert "delete NRIUturnMVC"
    This reverts commit 5dcc593f86265a360e933ed016c8ae0e0bfafd05.

---
+++ NRIUturnMVC/NRIUturnMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NRIUturnMVC.Startup))]
+namespace NRIUturnMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
